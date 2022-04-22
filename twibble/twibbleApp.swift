9ecaeab5cf5bf47f2594644c1a5e3a5fa6a41742
Venturh <venturh94@gmail.com>
2022-04-22 15:22:05 +0200

    Initial Commit

---
+++ twibble/twibbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  twibbleApp.swift
+//  twibble
+//
+//  Created by Max on 22.04.22.
+//
+
+import SwiftUI
+
+@main
+struct twibbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
